e9349abbb4bd6a215f43d2590f3a21287f5e10de
Jamie Knoef <jamie.knoef@gmail.com>
2017-05-31 11:05:19 +0200

    create patient testing

--- EPD/scripts/start-hyperledger.sh
+++ EPD/scripts/start-hyperledger.sh
@@ -10,7 +10,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 cd "${DIR}"/hlfv1
 
 docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml kill
 docker-compose -f docker-compose.yml up -d
 
 node create-channel.js
